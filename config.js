50b4a68ff776ff96f87d3f6ac5453eda41d63f46
divrawat <divrawat2001@gmail>
2024-06-15 17:12:57 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,492 @@
+
+export const DOMAIN = 'http://localhost:3000';
+// export const DOMAIN = 'https://www.vinlandsagamanga.in.net';
+
+export const MANGA_DESCRIPTION = `<a href="${DOMAIN}"><u>Boruto: Naruto Next Generations</u></a> is an anime series that follows the adventures of Boruto Uzumaki, the son of Naruto Uzumaki, the Seventh Hokage of the Hidden Leaf Village. The story explores Boruto's struggles to step out of his father's shadow while forging his own ninja path. Alongside his friends Sarada Uchiha and Mitsuki, Boruto faces various challenges, including mysterious threats to their village and the ninja world.`
+
+export const MANGA_NAME = 'Boruto';
+export const MANGA_AUTHOR = 'Ukyō Kodachi';
+export const MANGA_RELEASE = '2016';
+export const MANGA_STATUS = 'Ongoing';
+export const MANGA_ARTIST = 'Masashi Kishimoto';
+export const MANGA_STUDIO = 'Studio Pierrot';
+export const MANGA_GENRE = 'Action, Adventure, Shonen, Super Power, Martial Arts';
+
+export const CHAPTER_PREFIX = 'boruto-chapter';
+export const IMAGE_PREFIX = 'images/boruto';
+export const NEXT_PREVIOUS_PREFIX = 'manga/boruto-chapter';
+export const URL_PREFIX = 'manga/boruto-chapter';
+
+export const AUTHOR_PAGE = `${DOMAIN}/boruto-team`;
+export const LOGO_URL = `${DOMAIN}/logo.webp`;
+export const COVER_IMG = `${DOMAIN}/cover.webp`;
+export const BEHIND_COVER_IMG = "https://static1.cbrimages.com/wordpress/wp-content/uploads/2024/01/boruto-two-blue-vortex.jpg";
+
+
+// Navbar
+export const logo = <img src={`${LOGO_URL}`} alt="Logo" className="h-[75px] w-[155px] mr-5 md:ml-0 ml-4 md:pb-2 md:my-0 my-1" />
+
+export const APP_NAME = 'Boruto';
+export const NavbarName = "Boruto";
+
+export const APP_DESCRIPTION = `Read ${MANGA_NAME} manga online at ${DOMAIN}. Enjoy high-quality scans, latest chapters, and connect with fans in our vibrant community. Dive into the epic Viking adventure today!`;
+
+export const DISQUS_SHORTNAME = "my-cms-7";
+
+
+export const MANGA_SUMMARY = [
+    {
+        id: 1,
+        content: "Boruto: Naruto Next Generations is a continuation of the renowned 'Naruto' series, shifting the focus to the next generation of ninja. The story centers on Boruto Uzumaki, the son of Naruto Uzumaki, the Seventh Hokage, and Hinata Hyuga. Unlike his father, who grew up as an orphan and struggled to gain recognition, Boruto starts his life with the burden of his father's immense legacy."
+    },
+    {
+        id: 2,
+        content: "Boruto, feeling overshadowed by his father's accomplishments and the expectations placed on him, initially resents Naruto's dedication to his Hokage duties, believing it causes neglect of their family. This familial tension drives Boruto's early development and motivations. He seeks to establish his identity, separate from his father's, while navigating the trials of being a young ninja in a peaceful but evolving world."
+    },
+    {
+        id: 3,
+        content: "Joining Boruto in his adventures are his close friends and teammates, Sarada Uchiha and Mitsuki. Sarada, the daughter of Sasuke Uchiha and Sakura Haruno, aspires to become Hokage herself and grapples with her own family dynamics and the legacy of the Uchiha clan. Mitsuki, a synthetic human created by the enigmatic Orochimaru, seeks to understand his place in the world and the nature of human emotions."
+    },
+    {
+        id: 4,
+        content: "The series delves into various arcs that introduce new villains and complex challenges. One of the central antagonists is the Otsutsuki clan, a celestial family with god-like powers and a direct connection to the ancient roots of the ninja world. Their plans threaten global peace and bring Boruto and his friends into conflicts that test their abilities and resolve."
+    },
+    {
+        id: 5,
+        content: "A significant subplot involves Boruto's acquisition of a mysterious mark known as the Karma seal, which grants him extraordinary abilities but also ties him to the Otsutsuki clan's sinister plans. This mark becomes a pivotal element in Boruto's journey, symbolizing both a powerful tool and a dangerous curse."
+    },
+    {
+        id: 6,
+        content: "Throughout the series, themes of legacy, destiny, and personal growth are explored. Boruto's journey is not only about overcoming external threats but also about understanding the value of the bonds he forms with his friends and family. The anime also reflects on the changing nature of the ninja world, highlighting the balance between tradition and progress."
+    },
+    {
+        id: 6,
+        content: "Boruto: Naruto Next Generations balances action-packed battles with heartfelt moments, emphasizing the importance of perseverance, loyalty, and self-discovery. It builds on the rich lore established in 'Naruto' while introducing fresh perspectives and new generations of characters, ensuring that the spirit of the ninja way continues to captivate audiences."
+    },
+];
+
+
+
+
+export const navLinks = [
+    { text: 'Home', href: `${DOMAIN}` },
+    { text: 'DMCA', href: `${DOMAIN}/dmca` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` },
+];
+
+export const FooterLinks = [
+    { text: 'About', href: `${DOMAIN}/about` },
+    { text: 'Contact', href: `${DOMAIN}/contact` },
+    { text: 'Disclaimer', href: `${DOMAIN}/disclaimer` },
+    { text: 'Privacy Policy', href: `${DOMAIN}/privacy-policy` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` }
+];
+
+
+export const RelatedMangaLinks = [
+    {
+        imageUrl: `${DOMAIN}/related/frieran.webp`,
+        title: 'Frieran',
+        link: 'https://www.frieranmanga.in.net',
+    },
+    {
+        imageUrl: `${DOMAIN}/related/one-punch-man.webp`,
+        title: 'One Punch Man',
+        link: 'https://www.onepunchmanmanga.in.net',
+    },
+    {
+        imageUrl: `${DOMAIN}/related/vinland-saga.webp`,
+        title: 'Vinland Saga',
+        link: 'https://www.vinlandsagamanga.in.net',
+    },
+    {
+        imageUrl: `${DOMAIN}/related/jujutsu-kaisen.webp`,
+        title: 'Jujutsu Kaisen',
+        link: 'https://www.jujutsukaisenmanga.in.net',
+    },
+    {
+        imageUrl: `${DOMAIN}/related/black-clover.webp`,
+        title: 'Black Clover',
+        link: 'https://www.blackclovermanga.in.net',
+    },
+    {
+        imageUrl: `${DOMAIN}/related/chainsaw-man.webp`,
+        title: 'Chainsaw Man',
+        link: 'https://www.chainsawmanmanga.in.net',
+    },
+    {
+        imageUrl: `${DOMAIN}/related/mushoku-tensei.webp`,
+        title: 'Mushoku Tensei',
+        link: 'https://www.mushokutenseimanga.in.net',
+    },
+];
+
+
+
+
+export const chaptersData = [
+    {
+        "chapterNumber": "1",
+        "numImages": 64
+    },
+    {
+        "chapterNumber": "2",
+        "numImages": 46
+    },
+    {
+        "chapterNumber": "3",
+        "numImages": 48
+    },
+    {
+        "chapterNumber": "3a",
+        "numImages": 44
+    },
+    {
+        "chapterNumber": "4",
+        "numImages": 52
+    },
+    {
+        "chapterNumber": "5",
+        "numImages": 48
+    },
+    {
+        "chapterNumber": "6",
+        "numImages": 46
+    },
+    {
+        "chapterNumber": "7",
+        "numImages": 23
+    },
+    {
+        "chapterNumber": "8",
+        "numImages": 45
+    },
+    {
+        "chapterNumber": "9",
+        "numImages": 46
+    },
+    {
+        "chapterNumber": "10",
+        "numImages": 46
+    },
+    {
+        "chapterNumber": "11",
+        "numImages": 46
+    },
+    {
+        "chapterNumber": "12",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "13",
+        "numImages": 47
+    },
+    {
+        "chapterNumber": "14",
+        "numImages": 48
+    },
+    {
+        "chapterNumber": "15",
+        "numImages": 48
+    },
+    {
+        "chapterNumber": "16",
+        "numImages": 48
+    },
+    {
+        "chapterNumber": "17",
+        "numImages": 44
+    },
+    {
+        "chapterNumber": "18",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "19",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "20",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "21",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "22",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "23",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "24",
+        "numImages": 42
+    },
+    {
+        "chapterNumber": "25",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "26",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "27",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "28",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "29",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "30",
+        "numImages": 39
+    },
+    {
+        "chapterNumber": "31",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "32",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "33",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "34",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "35",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "36",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "37",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "38",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "39",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "40",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "41",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "42",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "43",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "44",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "45",
+        "numImages": 39
+    },
+    {
+        "chapterNumber": "46",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "47",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "48",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "49",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "50",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "51",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "52",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "53",
+        "numImages": 39
+    },
+    {
+        "chapterNumber": "54",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "55",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "56",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "57",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "58",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "59",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "60",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "61",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "62",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "63",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "64",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "65",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "66",
+        "numImages": 39
+    },
+    {
+        "chapterNumber": "67",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "68",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "69",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "70",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "71",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "72",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "73",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "74",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "75",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "76",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "77",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "78",
+        "numImages": 42
+    },
+    {
+        "chapterNumber": "79",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "80",
+        "numImages": 44
+    },
+    {
+        "chapterNumber": "81",
+        "numImages": 51
+    },
+    {
+        "chapterNumber": "82",
+        "numImages": 52
+    },
+    {
+        "chapterNumber": "83",
+        "numImages": 44
+    },
+    {
+        "chapterNumber": "84",
+        "numImages": 42
+    },
+    {
+        "chapterNumber": "85",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "86",
+        "numImages": 42
+    },
+    {
+        "chapterNumber": "87",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "88",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "89",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "90",
+        "numImages": 41
+    },
+];
